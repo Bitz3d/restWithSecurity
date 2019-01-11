5a3bcc143f903ffa3f3cfd4176893d6d76eee9d8
Bitz3d <raf.cieslinski@gmail.com>
2019-01-11 22:46:04 +0100

    models

---
+++ src/main/java/pl/rafalab/restWithSpring/Model/User.java
@@ -0,0 +1,40 @@
+package pl.rafalab.restWithSpring.Model;
+
+import java.sql.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@NotNull
+	private String name;
+	@NotEmpty(message="Pole nie może być puste")
+	private String email;
+	@NotNull
+	private String lastName;
+	@NotNull
+	private String password;
+	@NotNull
+	private String gender;
+	@ManyToMany(cascade=CascadeType.ALL)
+	private Set<Role> roles;
+	@NotNull
+	private String country;
+	@NotNull
+	private Date birthDate;
+}
