860092c31d246fa11d0f4539f8ee4cb1317271b8
Bitz3d <raf.cieslinski@gmail.com>
2019-01-11 18:58:09 +0100

    first commit

---
+++ src/main/java/pl/rafalab/restWithSpring/RestWithSpringApplication.java
@@ -0,0 +1,14 @@
+package pl.rafalab.restWithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringApplication.class, args);
+	}
+
+}
+
